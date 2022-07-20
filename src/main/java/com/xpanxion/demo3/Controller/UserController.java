8e4500815ffe0fc695eb86eca04e4a293364cdd5
mpereira0726 <106694216+mpereira0726@users.noreply.github.com>
2022-07-20 17:49:34 -0400

    first commit

---
+++ src/main/java/com/xpanxion/demo3/Controller/UserController.java
@@ -0,0 +1,28 @@
+package com.xpanxion.demo3.Controller;
+
+import com.xpanxion.demo3.Model.User;
+import com.xpanxion.demo3.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path="/User")
+public class UserController {
+    @Autowired
+    private com.example.accessingdatamysql.UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email){
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
